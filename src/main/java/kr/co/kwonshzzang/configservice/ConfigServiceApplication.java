8ba5bf943e69eb586d89e439a86937708c4367c2
kwonshzzang <kwonshzzang@gmail.com>
2024-09-08 16:51:56 +0900

    Add configuration

--- src/main/java/kr/co/kwonshzzang/configservice/ConfigServiceApplication.java
+++ src/main/java/kr/co/kwonshzzang/configservice/ConfigServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableConfigServer // 스프링 부트 애플리케이션에서 설정 서버 활성화
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
